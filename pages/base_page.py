17000beb563628ff9514816d9c324ef8e2fd3bdd
Sergei Kuznetsov <segakuz@gmail.com>
2021-10-26 12:18:11 +0500

    refactor(app): move login test methods to BASE

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 import math
 
 from training_project.pages.locators import ProductPageLocators
@@ -53,4 +54,10 @@ class BasePage():
             return False
 
         return True
-        
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -18,7 +18,6 @@ def test_guest_cant_see_success_message_after_adding_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    # page.solve_quiz_and_get_code()
     page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
@@ -32,5 +31,16 @@ def test_success_message_disappeared_after_adding_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    # page.solve_quiz_and_get_code()
     page.should_disappear_of_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
