5552651cfa1daaf7e00c72e23a5ba7c1986cbc96
Sergei Kuznetsov <segakuz@gmail.com>
2021-10-27 15:08:42 +0500

    refactor(prodcut page): decrease amount of promo offers

--- test_product_page.py
+++ test_product_page.py
@@ -5,7 +5,7 @@ import pytest
 
 @pytest.mark.need_review
 @pytest.mark.long
-@pytest.mark.parametrize('promo_offer', [pytest.param(i, marks=pytest.mark.xfail(i==7, reason='Temporary bug')) for i in range(10)])
+@pytest.mark.parametrize('promo_offer', [pytest.param(i, marks=pytest.mark.xfail(i==7, reason='Temporary bug')) for i in range(1)])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
